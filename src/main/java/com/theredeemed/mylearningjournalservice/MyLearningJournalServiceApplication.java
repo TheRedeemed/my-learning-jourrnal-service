9c9c0d6acedd92356669beea70058d453f3865d0
TheRedeemed <abdoulkaders77@gmail.com>
2020-07-24 22:53:17 -0400

    first commit

---
+++ src/main/java/com/theredeemed/mylearningjournalservice/MyLearningJournalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.theredeemed.mylearningjournalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearningJournalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLearningJournalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theredeemed/mylearningjournalservice/MyLearningJournalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theredeemed.mylearningjournalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLearningJournalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
